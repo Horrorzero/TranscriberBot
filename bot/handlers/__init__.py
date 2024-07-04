215ac56d5953786f4542265e1b6d841c60982f8e
Horrorzero <vladpuhskar82@gmail.com>
2024-07-04 09:46:06 +0300

    removed unimportant parts

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,7 +2,7 @@ from aiogram import Router
 
 from bot.handlers.start import router as start_router
 from bot.handlers.help import router as help_router
-from bot.handlers.stt import router as stt_tts_router
+from bot.handlers.stt import router as stt_router
 from bot.handlers.lang import router as lang_router
 from bot.handlers.checker import router as checker_router
 from bot.logger import get_logger
@@ -13,5 +13,5 @@ router = Router()
 logger = get_logger(__name__)
 
 router.include_routers(
-    start_router, help_router, stt_tts_router, lang_router, checker_router
+    start_router, help_router, stt_router, lang_router, checker_router
 )
